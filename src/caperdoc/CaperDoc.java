fe3fa7a041cc67e180bc723b94fe97656bf4118a
buaaaaang <buaaaaang@kaist.ac.kr>
2021-12-02 04:09:39 +0900

    hello world

--- src/caperdoc/CaperDoc.java
+++ src/caperdoc/CaperDoc.java
@@ -2,11 +2,8 @@ package caperdoc;
 
 public class CaperDoc {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
