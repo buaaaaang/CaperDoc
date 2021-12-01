94123ff734f2aaf1ba6adfb96563d2d412e85d6f
buaaaaang <69184903+buaaaaang@users.noreply.github.com>
2021-12-02 04:06:03 +0900

    first commit

---
+++ src/caperdoc/CaperDoc.java
@@ -0,0 +1,12 @@
+package caperdoc;
+
+public class CaperDoc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
