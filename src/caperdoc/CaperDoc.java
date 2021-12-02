19467ce4bcf8f77925f004dc2bfbfbb31c77bf22
buaaaaang <buaaaaang@kaist.ac.kr>
2021-12-02 19:11:02 +0900

    added frame

--- src/caperdoc/CaperDoc.java
+++ src/caperdoc/CaperDoc.java
@@ -1,9 +1,35 @@
 package caperdoc;
 
-public class CaperDoc {
+import javax.swing.JFrame;
+import x.XApp;
+import x.XLogMgr;
+import x.XScenarioMgr;
 
+public class CaperDoc extends XApp {
+    // fields
+    private JFrame mFrame = null;
+    
+    @Override
+    public XScenarioMgr getScenarioMgr() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public XLogMgr getXLogMgr() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    // constructor
+    public CaperDoc() {
+        this.mFrame = new JFrame("CaperDoc");
+        this.mFrame.setSize(800, 600);
+        this.mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.mFrame.setVisible(true);
+    }
+    
+    // main
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        CaperDoc myCaperDoc = new CaperDoc();
     }
     
 }
