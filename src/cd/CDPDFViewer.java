0538afed4860a1759c7c357c1e5e148077ad800d
buaaaaang <buaaaaang@kaist.ac.kr>
2021-12-04 16:48:45 +0900

    Move implemented

--- src/cd/CDPDFViewer.java
+++ src/cd/CDPDFViewer.java
@@ -5,20 +5,17 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JPanel;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.rendering.PDFRenderer;
-import org.apache.pdfbox.rendering.RenderDestination;
 
 public class CDPDFViewer extends JPanel {
     // constants
-    private static final float WIDTH_PDF_RATIO = 0.95f;
-    private static final float PDF_LOCATION_RATIO = 0.025f;
-    private static final int PDF_PAGE_GAP = 10;
+    private static final float RATIO_PDF_WIDTH = 0.98f;
+    private static final float RATIO_PDF_LOCATION = 0.01f;
+    private static final float RATIO_PAGE_GAP = 0.01f;
     
     // fields
     private CD mCD = null;
@@ -51,10 +48,10 @@ public class CDPDFViewer extends JPanel {
         PDRectangle singlePage = page.getCropBox();
         float pageWidth = singlePage.getWidth();
         float pageHeight = singlePage.getHeight();
-        float scale = this.getWidth() * CDPDFViewer.WIDTH_PDF_RATIO / pageWidth;
+        float scale = this.getWidth() * CDPDFViewer.RATIO_PDF_WIDTH / pageWidth;
         int nPages = (int) (this.getHeight() / (pageHeight * scale)) + 2;
         
-        int xPos = (int) ((this.getWidth() * CDPDFViewer.PDF_LOCATION_RATIO));
+        int xPos = (int) ((this.getWidth() * CDPDFViewer.RATIO_PDF_LOCATION));
         int yPos = -1 * ((int) (pageHeight * scale * this.mPageLocation));
     
         for (int i = 0; i < nPages; i++) {
@@ -62,10 +59,18 @@ public class CDPDFViewer extends JPanel {
                 BufferedImage pageImage = 
                     this.mRenderer.renderImage(this.mPage + i, scale);
                 g2.drawImage(pageImage, xPos, yPos, null);
-                yPos += pageHeight * scale + CDPDFViewer.PDF_PAGE_GAP;
+                yPos += pageHeight * scale * (1 + CDPDFViewer.RATIO_PAGE_GAP);
             } catch (IOException e) { }
         }
     }
     
+    public void Move(float f) {
+        this.mPageLocation += f;
+        if (mPageLocation < 0 || mPageLocation >= 1) {
+            mPage += Math.floor(mPageLocation);
+            this.mPageLocation = this.mPageLocation - 
+                (float) Math.floor(this.mPageLocation);
+        }
+    }
     
 }
